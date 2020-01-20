451eb4e5716d91f49355bb8e71c2615286ea3e2a
b7c <58299468+b7c@users.noreply.github.com>
2020-01-21 12:14:07 +1300

    Add IInterceptor.cs

---
+++ xabbo-core/Interceptor/IInterceptor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Xabbo.Core.Messages;
+using Xabbo.Core.Protocol;
+
+namespace Xabbo.Core
+{
+    public interface IInterceptor
+    {
+        MessageDispatcher Dispatcher { get; }
+        Task<bool> SendToServerAsync(short header, params object[] values);
+        Task<bool> SendToServerAsync(Packet packet);
+        Task<bool> SendToServerAsync(byte[] data);
+        Task<bool> SendToClientAsync(short header, params object[] values);
+        Task<bool> SendToClientAsync(Packet packet);
+        Task<bool> SendToClientAsync(byte[] data);
+    }
+}
