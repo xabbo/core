f0b2b123d395ef6b478927c996130a56d9c52193
b7c <58299468+b7c@users.noreply.github.com>
2020-01-23 17:52:00 +1300

    Add ToShortString for FurniType

--- xabbo-core/Room/Furni/FurniType.cs
+++ xabbo-core/Room/Furni/FurniType.cs
@@ -7,4 +7,17 @@ namespace Xabbo.Core
         Floor,
         Wall
     }
+
+    public static partial class XabboEnumExtensions
+    {
+        public static string ToShortString(this FurniType type)
+        {
+            switch (type)
+            {
+                case FurniType.Floor: return "S";
+                case FurniType.Wall: return "I";
+                default: throw new Exception($"Unknown furni type: {type}");
+            }
+        }
+    }
 }
