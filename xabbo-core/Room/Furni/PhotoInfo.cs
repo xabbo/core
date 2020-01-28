35ef9e993256bc3d833288893febac578522758a
b7c <58299468+b7c@users.noreply.github.com>
2020-01-28 16:59:51 +1300

    Add PhotoInfo

---
+++ xabbo-core/Room/Furni/PhotoInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Xabbo.Core
+{
+    public class PhotoInfo
+    {
+        [JsonProperty("t")]
+        public ulong Time { get; set; }
+        
+        [JsonProperty("id")]
+        public string Id { get; set; }
+    }
+}
