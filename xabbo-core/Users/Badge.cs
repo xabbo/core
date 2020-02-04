1dbe065fcb476d7a07277636c2e10f0acdb1f8cd
b7c <58299468+b7c@users.noreply.github.com>
2020-02-04 14:51:24 +1300

    Add Badge, GetBadgesTask

---
+++ xabbo-core/Users/Badge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xabbo.Core.Protocol;
+
+namespace Xabbo.Core
+{
+    public class Badge
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+
+        public Badge() { }
+
+        public Badge(int id, string code)
+        {
+            Id = id;
+            Code = code;
+        }
+
+        protected Badge(Packet packet)
+        {
+            Id = packet.ReadInteger();
+            Code = packet.ReadString();
+        }
+
+        public static Badge Parse(Packet packet) => new Badge(packet);
+    }
+}
