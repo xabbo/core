47bf2419feea9bc9429f57a5773513b2aae60e47
b7c <58299468+b7c@users.noreply.github.com>
2020-12-08 19:02:38 +1300

    Add TradeStartError

---
+++ xabbo-core/Enum/TradeStartError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Xabbo.Core
+{
+    public enum TradeStartError
+    {
+        TradingDisabledHotel = 1,
+        TradingDisabledAccount = 2,
+        TradingDisabledPartner = 4,
+        TradingDisabledRoom = 6,
+        OngoingTrade = 7,
+        PartnerOngoingTrade = 8
+    }
+}
