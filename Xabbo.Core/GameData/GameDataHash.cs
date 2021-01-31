4ba27dd6c861fa90dde1507f433e31ad45ca54be
b7c <58299468+b7c@users.noreply.github.com>
2021-01-31 23:29:02 +1300

    Add game data hashes

---
+++ Xabbo.Core/GameData/GameDataHash.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Xabbo.Core.GameData
+{
+    public class GameDataHash
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Hash { get; set; }
+
+        public GameDataHash()
+        {
+            Name = string.Empty;
+            Url = string.Empty;
+            Hash = string.Empty;
+        }
+    }
+}
---
+++ Xabbo.Core/GameData/GameDataHashesContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Xabbo.Core.GameData
+{
+    public class GameDataHashesContainer
+    {
+        public List<GameDataHash> Hashes { get; set; }
+
+        public GameDataHashesContainer()
+        {
+            Hashes = new List<GameDataHash>();
+        }
+    }
+}
