4d37bab8dbc1aa293d50c892ea0f4490329828e0
b7c <58299468+b7c@users.noreply.github.com>
2022-01-05 04:47:06 +1300

    add game data manager

---
+++ Xabbo.Core/GameData/GameDataManager.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xabbo.Core.Web;
+
+namespace Xabbo.Core.GameData
+{
+    public class GameDataManager : IGameDataManager
+    {
+        private readonly SemaphoreSlim _loadSemaphore = new(1, 1);
+
+        private TaskCompletionSource? _tcsLoad;
+        private Task _loadTask;
+
+        public string CachePath { get; }
+
+        public FigureData? Figure { get; private set; }
+        public FurniData? Furni { get; private set; }
+        public ProductData? Products { get; private set; }
+        public ExternalTexts? Texts { get; private set; }
+
+        public event Action? Loading;
+        public event Action? Loaded;
+        public event Action<Exception>? LoadFailed;
+
+        public GameDataManager()
+            : this(@"%APPDATA%\xabbo\cache\gamedata")
+        { }
+
+        public GameDataManager(string cachePath)
+        {
+            _tcsLoad = new(TaskCreationOptions.RunContinuationsAsynchronously);
+            _loadTask = _tcsLoad.Task;
+
+            CachePath = Path.GetFullPath(Environment.ExpandEnvironmentVariables(cachePath));
+        }
+
+        private void Reset()
+        {
+            Figure = null;
+            Furni = null;
+            Products = null;
+            Texts = null;
+        }
+
+        private async Task LoadDataAsync(
+            HttpClient http,
+            string domain,
+            GameDataType type,
+            string url,
+            string hash,
+            CancellationToken cancellationToken)
+        {
+            string cacheFolderPath = Path.Combine(CachePath, domain, type.ToString().ToLower());
+
+            string filePath = Path.Combine(cacheFolderPath, hash);
+
+            if (!File.Exists(filePath))
+            {
+                HttpResponseMessage response = await http.GetAsync($"{url}/{hash}", cancellationToken);
+                response.EnsureSuccessStatusCode();
+
+                using var outs = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite);
+                await response.Content.CopyToAsync(outs, cancellationToken);
+            }
+
+            switch (type)
+            {
+                case GameDataType.Furni: Furni = FurniData.LoadJsonFile(filePath); break;
+                case GameDataType.Product: Products = ProductData.LoadJsonFile(filePath); break;
+                case GameDataType.Texts: Texts = ExternalTexts.Load(filePath); break;
+                case GameDataType.Figure: Figure = FigureData.LoadXml(filePath); break;
+                default: break;
+            }
+        }
+
+        public async Task LoadAsync(string domain, CancellationToken cancellationToken)
+        {
+            if (!_loadSemaphore.Wait(0, cancellationToken))
+                throw new InvalidOperationException("Game data is currently being loaded.");
+
+            try
+            {
+                if (_tcsLoad is null)
+                {
+                    _tcsLoad = new(TaskCreationOptions.RunContinuationsAsynchronously);
+                    _loadTask = _tcsLoad.Task;
+                }
+
+                Reset();
+                Loading?.Invoke();
+
+                using var http = new HttpClient();
+                http.DefaultRequestHeaders.Add("User-Agent", H.UserAgent);
+
+                GameDataHashesContainer? hashesContainer = null;
+
+                FileInfo hashesFile = new(Path.Combine(CachePath, domain, "hashes.json"));
+                if (hashesFile.Exists &&
+                    (DateTime.Now - hashesFile.LastWriteTime) < TimeSpan.FromMinutes(15))
+                {
+                    hashesContainer = JsonSerializer.Deserialize<GameDataHashesContainer>(
+                        await File.ReadAllTextAsync(hashesFile.FullName, cancellationToken)
+                    );
+                }
+                else
+                {
+                    string json = await http.GetStringAsync(
+                        $"https://www.habbo.{domain}/gamedata/hashes2",
+                        cancellationToken
+                    );
+                    hashesContainer = JsonSerializer.Deserialize<GameDataHashesContainer>(json);
+
+                    await File.WriteAllTextAsync(hashesFile.FullName, json, cancellationToken);
+                }
+
+                if (hashesContainer is null)
+                    throw new Exception("Failed to deserialize game data hashes.");
+
+                List<Task> loadTasks = new();
+
+                foreach (GameDataHash entry in hashesContainer.Hashes)
+                {
+                    GameDataType type = entry.Name switch
+                    {
+                        "furnidata" => GameDataType.Furni,
+                        "productdata" => GameDataType.Product,
+                        "external_texts" => GameDataType.Texts,
+                        "figurepartlist" => GameDataType.Figure,
+                        _ => (GameDataType)(-1)
+                    };
+
+                    if (!Enum.IsDefined(type)) continue;
+
+                    loadTasks.Add(LoadDataAsync(http, domain, type, entry.Url, entry.Hash, cancellationToken));
+                }
+
+                await Task.WhenAll(loadTasks);
+
+                Loaded?.Invoke();
+                _tcsLoad.TrySetResult();
+            }
+            catch (Exception ex)
+            {
+                Reset();
+                LoadFailed?.Invoke(ex);
+                _tcsLoad?.TrySetException(ex);
+                throw;
+            }
+            finally
+            {
+                _tcsLoad = null;
+                _loadSemaphore.Release();
+            }
+        }
+
+        public async Task WaitForLoadAsync(CancellationToken cancellationToken)
+        {
+            await await Task.WhenAny(_loadTask, Task.Delay(-1, cancellationToken));
+        }
+    }
+}
---
+++ Xabbo.Core/GameData/IGameDataManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Xabbo.Core.GameData
+{
+    public interface IGameDataManager
+    {
+        /// <summary>
+        /// Gets the cache path where game data is stored.
+        /// </summary>
+        string CachePath { get; }
+
+        /// <summary>
+        /// Gets the figure data if it is available.
+        /// </summary>
+        FigureData? Figure { get; }
+        /// <summary>
+        /// Gets the furni data if it is available.
+        /// </summary>
+        FurniData? Furni { get; }
+        /// <summary>
+        /// Gets the product data if it is available.
+        /// </summary>
+        ProductData? Products { get; }
+        /// <summary>
+        /// Gets the external texts if they are available.
+        /// </summary>
+        ExternalTexts? Texts { get; }
+
+        /// <summary>
+        /// Invoked when game data begins loading.
+        /// </summary>
+        event Action Loading;
+
+        /// <summary>
+        /// Invoked when game data is successfully loaded.
+        /// </summary>
+        event Action Loaded;
+
+        /// <summary>
+        /// Invoked when game data fails to load.
+        /// </summary>
+        event Action<Exception> LoadFailed;
+
+        /// <summary>
+        /// Loads game data for the specified domain.
+        /// </summary>
+        Task LoadAsync(string domain, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Waits for game data to load.
+        /// </summary>
+        Task WaitForLoadAsync(CancellationToken cancellationToken);
+    }
+}
