3bd909ff99d52f240854df9d4aef32bba5190511
b7c <58299468+b7c@users.noreply.github.com>
2022-07-14 23:36:24 +1200

    More documentation

--- Xabbo.Core/Room/WallOrientation.cs
+++ Xabbo.Core/Room/WallOrientation.cs
@@ -2,14 +2,34 @@
 
 namespace Xabbo.Core
 {
+    /// <summary>
+    /// Represents a wall orientation.
+    /// </summary>
     public struct WallOrientation
     {
-        public static readonly WallOrientation Left = new WallOrientation('l');
-        public static readonly WallOrientation Right = new WallOrientation('r');
+        /// <summary>
+        /// Represents a left wall orientation.
+        /// </summary>
+        public static readonly WallOrientation Left = new('l');
 
+        /// <summary>
+        /// Represents a left wall orientation.
+        /// </summary>
+        public static readonly WallOrientation Right = new('r');
+
+        /// <summary>
+        /// Gets the character value of this wall orientation.
+        /// </summary>
         public char Value { get; }
 
+        /// <summary>
+        /// Gets if this is a left wall orientation.
+        /// </summary>
         public bool IsLeft => Value == 'l';
+
+        /// <summary>
+        /// Gets if this is a right wall orientation.
+        /// </summary>
         public bool IsRight => Value == 'r';
 
         private WallOrientation(char value)
@@ -19,13 +39,17 @@ namespace Xabbo.Core
 
         public override string ToString() => Value.ToString();
 
+        /// <summary>
+        /// Gets the wall orientation by the specified character. Must be <c>l</c> or <c>r</c>.
+        /// </summary>
+        /// <exception cref="ArgumentException">The specified character is not a valid wall orientation.</exception>
         public static WallOrientation FromChar(char c)
         {
             return c switch
             {
                 'l' or 'L' => Left,
                 'r' or 'R' => Right,
-                _ => throw new InvalidCastException($"Invalid wall orientation '{c}', must be 'l' or 'r'"),
+                _ => throw new ArgumentException($"Invalid wall orientation '{c}'. Must be 'l' or 'r'."),
             };
         }
 
