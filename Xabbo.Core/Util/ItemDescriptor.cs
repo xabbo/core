9061aa05372f2993cdf6077a6c0d0ab51378a3ba
b7c <58299468+b7c@users.noreply.github.com>
2022-08-01 02:30:11 +1200

    move ItemDescriptor to Util folder

--- Xabbo.Core/Util/ItemDescriptor.cs
+++ Xabbo.Core/Util/ItemDescriptor.cs
@@ -13,24 +13,22 @@ public struct ItemDescriptor : IItem
     /// Gets the item type.
     /// </summary>
     public ItemType Type { get; }
+
     /// <summary>
     /// Gets the item kind.
     /// </summary>
     public int Kind { get; }
+
     /// <summary>
     /// Gets the item variant.
     /// </summary>
-    public string Variant { get; }
+    public string? Variant { get; }
 
-    public ItemDescriptor(ItemType type, int kind)
-        : this(type, kind, null)
-    { }
-
-    public ItemDescriptor(ItemType type, int kind, string? variant)
+    public ItemDescriptor(ItemType type, int kind, string? variant = null)
     {
         Type = type;
         Kind = kind;
-        Variant = variant ?? string.Empty;
+        Variant = variant;
     }
 
     public override int GetHashCode() => (Type, Kind, Variant).GetHashCode();
@@ -49,6 +47,16 @@ public struct ItemDescriptor : IItem
             Variant == other.Variant;
     }
 
+    public override string ToString() => Type switch
+    {
+        ItemType.Floor or ItemType.Wall => !string.IsNullOrWhiteSpace(Variant) ? $"{Type}:{Kind}:{Variant}" : $"{Type}:{Kind}",
+        ItemType.Badge or ItemType.Effect or ItemType.Bot => $"{Type}:{Variant ?? "?"}",
+        _ => $"{Type}:{Kind}:{Variant ?? "?"}"
+    };
+
     public static bool operator ==(ItemDescriptor a, ItemDescriptor b) => a.Equals(b);
     public static bool operator !=(ItemDescriptor a, ItemDescriptor b) => !(a == b);
+
+    public static implicit operator ItemDescriptor((ItemType Type, int Kind, string Variant) tuple) => new(tuple.Type, tuple.Kind, tuple.Variant);
+    public static implicit operator ItemDescriptor((ItemType Type, int Kind) tuple) => new(tuple.Type, tuple.Kind);
 }
