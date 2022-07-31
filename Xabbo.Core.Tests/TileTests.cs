7667872b9f9c8e62e5c81a0d3df5e49b26726a8d
b7c <58299468+b7c@users.noreply.github.com>
2022-08-01 02:34:50 +1200

    add tests

---
+++ Xabbo.Core.Tests/TileTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Xabbo.Core.Tests;
+
+public class TileTests
+{
+    [Fact(DisplayName = "Tile == Tuple")]
+    public void Tile_Equals_Tuple()
+    {
+        Tile tile = new(1, 2, 3.4f);
+
+        // Compare X,Y,Z
+        Assert.True(tile == (1, 2, 3.4f));
+
+        // Compare X,Y only
+        Assert.True(tile == (1, 2));
+
+        // Compare Z with epsilon check
+        Assert.True(tile == (1, 2, 3.4001f));
+        Assert.True(tile == (1, 2, 3.401f));
+        Assert.False(tile == (1, 2, 3.41f));
+    }
+}
