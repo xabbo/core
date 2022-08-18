2effd1d17bc24e0d49738065be34a497e41cc05f
b7c <58299468+b7c@users.noreply.github.com>
2022-08-18 21:19:59 +1200

    simplify tests

--- Xabbo.Core.Tests/TileTests.cs
+++ Xabbo.Core.Tests/TileTests.cs
@@ -11,27 +11,16 @@ public class TileTests
 
         // Compare X,Y,Z
         Assert.True(tile == (1, 2, 3.4f));
-        Assert.False(tile != (1, 2, 3.4f));
-
         Assert.True((1, 2, 3.4f) == tile);
-        Assert.False((1, 2, 3.4f) != tile);
 
         // Compare X,Y only
         Assert.True(tile == (1, 2));
-        Assert.False(tile != (1, 2));
-
         Assert.True((1, 2) == tile);
-        Assert.False((1, 2) != tile);
 
         // Compare Z with epsilon check
         Assert.True(tile == (1, 2, 3.4001f));
-        Assert.False(tile != (1, 2, 3.4001f));
-
         Assert.True(tile == (1, 2, 3.401f));
-        Assert.False(tile != (1, 2, 3.401f));
-
         Assert.False(tile == (1, 2, 3.41f));
-        Assert.True(tile != (1, 2, 3.41f));
     }
 
     [Fact]
