4417100f1f854b9556d260920b2f543a3faea878
rob <rob@b7c.io>
2024-09-24 18:54:02 +1200

    add new-message script

---
+++ .scripts/new-message.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -e
+endl=$'\n'
+
+direction=$(gum choose --header="Direction?" Incoming Outgoing)
+gum style "Direction: ${direction}"
+
+case $direction in
+  Incoming)
+    shortDir=In
+    ;;
+  Outgoing)
+    shortDir=Out
+    ;;
+esac
+
+clientType=$(gum choose --header="Client type?" Any Modern Origins)
+gum style "Client type: ${clientType}"
+
+filePath="src/Xabbo.Core/Messages/${direction}"
+
+case $clientType in
+  Modern|Origins)
+    clientNamespace=".$clientType"
+    filePath="${filePath}/${clientType}"
+    ;;
+esac
+
+msgName=$(gum input --header="Message name:")
+if [[ $msgName != *Msg ]]; then
+  msgName="${msgName}Msg"
+fi
+gum style "Message name: ${msgName}"
+
+filePath="${filePath}/${msgName}.cs"
+if [[ -f "$filePath" ]]; then
+  gum style --foreground=9 "File already exists: ${filePath}"
+  exit 1
+fi
+
+if [[ -n $clientType ]]; then
+  supportedClients="static ClientType IMessage<$msgName>.SupportedClients => ClientType.$clientType;${endl}    "
+fi
+
+identifier=$(gum input --header="Identifier:" --placeholder="${msgName%Msg}")
+gum style "Identifier: ${identifier:=${msgName%Msg}}"
+
+# Output source
+
+cat << EOF > $filePath
+using Xabbo.Messages;
+using Xabbo.Messages.Flash;
+
+namespace Xabbo.Core.Messages.${direction}${clientNamespace};
+
+public sealed record $msgName : IMessage<$msgName>
+{
+    ${supportedClients}static Identifier IMessage<$msgName>.Identifier => $shortDir.$identifier;
+    static $msgName IParser<$msgName>.Parse(in PacketReader p)
+    {
+        throw new System.NotImplementedException();
+    }
+    void IComposer.Compose(in PacketWriter p)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+EOF
