20f1617cc3df712cfce95b74dc7dc39c669b9b0a
rob <rob@b7c.io>
2024-10-12 21:22:02 +1300

    add TrackingId to TalkMsg

--- src/Xabbo.Core/Messages/Outgoing/TalkMsg.cs
+++ src/Xabbo.Core/Messages/Outgoing/TalkMsg.cs
@@ -16,13 +16,15 @@ namespace Xabbo.Core.Messages.Outgoing;
 /// </summary>
 /// <param name="Message">The chat message content.</param>
 /// <param name="BubbleStyle">The chat bubble style. Applies to <see cref="ClientType.Modern"/> clients.</param>
-public sealed record TalkMsg(string Message, int BubbleStyle = 0) : IMessage<TalkMsg>
+/// <param name="TrackingId">The tracking ID of the message.</param>
+public sealed record TalkMsg(string Message, int BubbleStyle = 0, int TrackingId = -1) : IMessage<TalkMsg>
 {
     static Identifier IMessage<TalkMsg>.Identifier => Out.Chat;
 
     static TalkMsg IParser<TalkMsg>.Parse(in PacketReader p) => new(
         p.ReadString(),
-        p.Client is ClientType.Shockwave ? 0 : p.ReadInt()
+        p.Client is ClientType.Shockwave ? 0 : p.ReadInt(),
+        p.Client is ClientType.Shockwave ? -1 : p.ReadInt()
     );
 
     void IComposer.Compose(in PacketWriter p)
@@ -31,7 +33,7 @@ public sealed record TalkMsg(string Message, int BubbleStyle = 0) : IMessage<Tal
         if (p.Client is not ClientType.Shockwave)
         {
             p.WriteInt(BubbleStyle);
-            p.WriteInt(-1);
+            p.WriteInt(TrackingId);
         }
     }
 }
